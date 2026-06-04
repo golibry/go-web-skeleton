9083ddc847fd793110dc30b86197893d03c37e50
rsgcata <rsgcata@users.noreply.github.com>
2026-06-04 19:38:28 +0300

    simplified cli registry build

--- framework/cli/bootstrap.go
+++ framework/cli/bootstrap.go
@@ -1,31 +1,33 @@
 package cli
 
 import (
+	"fmt"
 	"log/slog"
 	"os"
 
 	"github.com/golibry/go-cli-command/cli"
 )
 
+func NewRegistry(commands []cli.Command) (*cli.CommandsRegistry, error) {
+	commandRegistry := cli.NewCommandsRegistry()
+
+	for _, cmd := range commands {
+		if err := commandRegistry.Register(cmd); err != nil {
+			return nil, fmt.Errorf("failed to register command: %w", err)
+		}
+	}
+
+	return commandRegistry, nil
+}
+
 func Bootstrap(
 	logger *slog.Logger,
 	availableCommands []cli.Command,
 ) {
-	// Create a command registry and register commands
-	commandRegistry := cli.NewCommandsRegistry()
-
-	for _, cmd := range availableCommands {
-		err := commandRegistry.Register(cmd)
-		if err != nil {
-			logger.Error(
-				"Failed to register command",
-				"command",
-				cmd.Id(),
-				"error",
-				err,
-			)
-			os.Exit(1)
-		}
+	commandRegistry, err := NewRegistry(availableCommands)
+	if err != nil {
+		logger.Error("Failed to register commands", "error", err)
+		os.Exit(1)
 	}
 
 	// Bootstrap and run the CLI application
